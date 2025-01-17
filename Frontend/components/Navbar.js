d17be3afe1316bac9618403bd359d17b61a5f754
Abhinav Rajput <abhinavrajput179@gmail.com>
2025-01-18 03:16:29 +0530

    Navbar-Abhinav(without animations)

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'
+import MyScroll from '/public/NavbarScroll.svg';
+import './Navbar.css';
 
 function Navbar() {
+  
+  const slideRin=(e)=>{
+    console.log('slidein')
+  }
+  const slideRout=(e)=>{
+    console.log('slideout')
+  }
   return (
-    <div>Navbar</div>
-  )
+    <div className='navbarscrolls'>
+      <button onMouseOver={slideRin} onMouseLeave={slideRout}><MyScroll /></button>
+      <button onMouseOver={slideRin} onMouseLeave={slideRout}><MyScroll /></button>
+      <button onMouseOver={slideRin} onMouseLeave={slideRout}><MyScroll /></button>
+      <button onMouseOver={slideRin} onMouseLeave={slideRout}><MyScroll /></button>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
