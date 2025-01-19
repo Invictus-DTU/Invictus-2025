08673badf741eeda8d6a4f7ab4ac8d251e94579e
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2025-01-19 16:39:00 +0530

    something changed

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -1,5 +1,6 @@
 import Scroll from '/public/scroll.svg';
 import './Navbar.css';
+import Link from 'next/link';
 
 function Navbar() {
 
@@ -21,12 +22,14 @@ function Navbar() {
   return (
     <div className='flex justify-center gap-12 fixed w-full'>
       {routes.map((route, index) => 
+      <Link key={index} href={`/comingsoon`}>
         <button key={index} className='relative -top-8 scale-90 duration-150 hover:-top-5'>
           <Scroll className='z-0 scale-90'/>
-          <p className='absolute bottom-6 left-7 right-0 z-10 text-black font-medium text-md'>
+          <p className='absolute bottom-6 left-10 right-0 z-10 text-black font-medium text-lg'>
             {route.name}
           </p>
         </button>
+        </Link>
       )}
     </div>
   );
--- Frontend/components/Terrain.js
+++ Frontend/components/Terrain.js
@@ -7,20 +7,25 @@ import { usePathname } from "next/navigation";
 function Terrain() {
   const path = usePathname()
 
-  console.log(path)
+  // const [gate, setGate] = useState(false)
 
-  const [gate, setGate] = useState(false)
   return (
     <div className={"fixed bottom-0 left-0 w-full z-0 border-black border-4 flex flex-col -space-y-9 items-center"}>
       <div className="rounded-full w-[35%] aspect-square bg-[#CB0305] absolute bottom-0 -z-10" />
-      <div className={'flex justify-center items-end gap-10'}>
+      <div className={'flex justify-center items-end gap-10 z-0'}>
         {/* <img className={styles.bgCircle} src="/bgcircle.png" alt="" /> */}
         {/* <img className={gate ? styles.footerGateBig : styles.footerGateSmall} onClick={() => setGate(!gate)} src="/dtu_gate2.png" alt="" />
         <img className={styles.footerTnp} src="/tnpblock1.png" alt="" /> */}
-        <img src="/dtu_gate2.png" alt="" className="w-36 brightness-0" />
-        <img src="/tnpblock1.png" alt="" className="w-36" />
+        <img src="/dtu_gate2.png" alt="" className={cn(
+          "w-32 brightness-0 duration-700 absolute left-0 bottom-16",
+          path === "/" && "w-1/2 brightness-100",
+          )} />
+        <img src="/tnpblock1.png" alt="" className={cn(
+          "w-36 duration-700",
+          path === "/comingsoon" && "w-1/2",
+          )} />
       </div>
-      <img className={'w-full h-24'} src="/terrain.png" alt="" />
+      <img className={'w-full h-24 z-10'} src="/terrain.png" alt="" />
     </div>
   );
 }
