163035c5773e1837c45c1826e5a13c1d8435bbf0
Tushar <aabisht2006@gmail.com>
2025-01-19 11:30:44 +0530

    footer added

--- Frontend/components/Terrain.js
+++ Frontend/components/Terrain.js
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import styles from "./terrain.module.css";
 
 function Terrain() {
+
+  const [gate, setGate] = useState(false)
   return (
-    <div>Terrain</div>
-  )
+    <div className={styles.footer}>
+      <img className={styles.footerBg} src="/terrain.png" alt="" />
+      <div className={styles.footerContent}>
+        <img className={styles.bgCircle} src="/bgcircle.png" alt="" />
+        <img className={gate ? styles.footerGateBig : styles.footerGateSmall} onClick={() => setGate(!gate)} src="/dtu_gate2.png" alt="" />
+        <img className={styles.footerTnp} src="/tnpblock1.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Terrain
+export default Terrain;
