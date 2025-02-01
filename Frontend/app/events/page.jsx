637b3a7671609b6cc7f0305e8b6cb53d6104ba7d
Tushar <aabisht2006@gmail.com>
2025-02-01 12:42:53 +0530

    events backend done

--- Frontend/app/events/page.jsx
+++ Frontend/app/events/page.jsx
@@ -1,19 +1,15 @@
 "use client";
 import EventCard from "@/components/EventCard";
-import React, { useRef, useState } from "react";
-import {
-  FaAngleDoubleRight,
-  FaAngleRight,
-  FaArrowDown,
-  FaArrowRight,
-  FaSearch,
-} from "react-icons/fa";
+import React, { useRef, useState, useEffect } from "react";
+import { FaArrowDown, FaSearch } from "react-icons/fa";
 
 function page() {
   const scrollRef = useRef(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
+
+  const [events, setEvents] = useState([]);
 
   const handleMouseDown = (e) => {
     if (!scrollRef.current) return;
@@ -33,47 +29,86 @@ function page() {
   const handleMouseUp = () => {
     setIsDragging(false);
   };
+
+  const fetchAllData = async () => {
+    const response = await fetch("http://localhost:8000/api/events", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    setEvents(data.events);
+  };
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
   return (
     <>
       <div className="visible-delay flex flex-col items-center sm:items-start justify-start text-black px-4 sm:px-24 pt-5 sm:pt-20">
         <div className="flex flex-col md:flex-row items-center justify-between w-full">
-          <h1 className="text-6xl sm:text-5xl md:text-5xl lg:text-6xl">
+          <h1
+            className="text-6xl sm:text-5xl md:text-5xl lg:text-6xl"
+            onClick={fetchAllData}
+          >
             Events
           </h1>
 
-          <div className="flex flex-row w-[100%] md:w-auto items-center justify-center md:justify-between">
-            <button className="flex flex-row items-center justify-between bg-black px-3 py-2 text-white rounded-md text-lg cursor-pointer">
-              <span className="mr-2">Filter</span>
-              <FaArrowDown size={20} />
-            </button>
-            <div className="flex flex-row items-center justify-between ml-2 px-3 py-2 text-lg  rounded-md text-white bg-black">
-              <FaSearch size={24} className="mr-3" />
-              <input placeholder="Search for events..." type="text" className="bg-transparent outline-none font-jmh" />
-            </div>
+          <div className="py-2 flex flex-row w-[100%] md:w-auto items-center justify-center md:justify-between">
+            <select className="mr-2 text-[12px] md:text-[15px] flex flex-row items-center justify-between bg-black px-3 py-3 text-white rounded-md cursor-pointer">
+              <option>Mode</option>
+              <option>Online</option>
+              <option>Offline</option>
+              <option>Hybrid</option>
+            </select>
+            <select className="mr-2 text-[12px] md:text-[15px] flex flex-row items-center justify-between bg-black px-3 py-3 text-white rounded-md cursor-pointer">
+              <option className="mr-2">Category</option>
+            </select>
+            <select className="flex text-[12px] md:text-[15px] flex-row items-center justify-between bg-black px-3 py-3 text-white rounded-md cursor-pointer">
+              <option>Date</option>
+              <option>15th Feb</option>
+              <option>16th Feb</option>
+              <option>17th Feb</option>
+              <option>18th Feb</option>
+            </select>
+          </div>
+          <div className="flex flex-row items-center justify-between ml-2 px-3 py-2 text-lg  rounded-md text-white bg-black">
+            <FaSearch size={24} className="mr-3" />
+            <input
+              placeholder="search for events..."
+              type="text"
+              className="text-[12px] md:text-[15px] bg-transparent outline-none font-jmh"
+            />
           </div>
         </div>
-      </div>
-      <div className="mt-4 flex flex-row items-center justify-start w-full">
-        <div
-          ref={scrollRef}
-          className="flex pl-2 flex-row select-none overflow-x-scroll no-scrollbar cursor-grab active:cursor-grabbing"
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-        >
-          <div className="min-w-[100px] " />
+        <div className="mt-4 flex flex-row items-center justify-start w-full">
+          <div
+            ref={scrollRef}
+            className="flex pl-2 flex-row select-none overflow-x-scroll no-scrollbar cursor-grab active:cursor-grabbing"
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+          >
+            <div className="min-w-[0px] md:min-w-[50px] sm:min-w-[100px]" />
+            {events.map((event, index) => (
+              <EventCard key={index} data={event} />
+            ))}
+            {/* <EventCard />
           <EventCard />
           <EventCard />
           <EventCard />
           <EventCard />
-          <EventCard />
-          <EventCard />
-        </div>
+          <EventCard /> */}
+          </div>
 
-        {/* <button className="bg-[#2F1414] absolute z-1 cursor-pointer mx-4 p-4 rounded-full right-0 shadow-sm">
+          {/* <button className="bg-[#2F1414] absolute z-1 cursor-pointer mx-4 p-4 rounded-full right-0 shadow-sm">
           <FaAngleDoubleRight color="white" size={50} />
           </button> */}
+        </div>
       </div>
     </>
   );
--- Frontend/components/EventCard.jsx
+++ Frontend/components/EventCard.jsx
@@ -1,42 +1,65 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { CgClose } from 'react-icons/cg'
 
-const EventCard = () => {
+const EventCard = ({ data }) => {
+
+  const [eventCard, setEventCard] = useState(false)
+
   return (
-    <div className='my-3 flex flex-col items-center justify-center min-w-[280px] w-[280px] min-h-[300px] pb-3 hover:scale-[1.02] hover:traslate-y-[10px] duration-[0.5s] rounded-md mr-10 relative drop-shadow-[4px_6px_4px_rgba(0,0,0,0.3)] animate-event-card'>
-        <img src="/event-demo-logo.png" className='w-[70%]' />
+    <>
+      {
+        eventCard && (
+          <>
+            <div className='event-popup-overlay min-w-[100%] min-h-[100%] cursor-default' onClick={() => setEventCard(false)} />
+            <div className='event-popup min-h-[500px] w-[70vw] cursor-default'>
+                <div className="relative w-full min-h-[500px]">
+                  <button onClick={() => setEventCard(false)} className='absolute top-0 right-0 m-3'><CgClose size={34} /></button>
+                </div>
+            </div>
+          </>
+        )
+      }
+      <div className='my-3 flex flex-col items-center justify-center min-w-[280px] w-[280px] min-h-[300px] pb-3 hover:scale-[1.02] hover:traslate-y-[10px] duration-[0.5s] rounded-md pt-2 mr-10 relative drop-shadow-[4px_6px_4px_rgba(0,0,0,0.3)] animate-event-card'>
+          <img src="https://drive.google.com/thumbnail?id=1bGMnFJrM9lfMkovJMld-k66MGQ3gLOzg" className='w-[40%] rounded-md' />
+          {/* <img src="/event-demo-logo.png" className='w-[70%]' /> */}
 
+          <h3 className="text-center text-2xl uppercase">{data.event_name}</h3>
+          <h3 className="text-center text-[12px] font-jmh">{data.category}</h3>
 
-        <h3 className="text-center text-2xl uppercase">Event Name</h3>
-        <h3 className="text-center text-[12px] font-jmh uppercase">Fun event</h3>
+          <p className='text-[10px] px-4 font-jmh mt-4 font-limerick'>
+            {data.event_desc}
+          </p>
 
-        <p className='text-[8px] px-4 font-jmh mt-4 font-limerick'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quibusdam cum dolorum ex eum animi voluptas at vero incidunt doloremque, magnam distinctio consectetur suscipit earum obcaecati minima quisquam optio harum repellendus quasi! Inventore, quibusdam temporibus.</p>
-
-        <div className='flex flex-row items-center justify-evenly w-[90%] mx-auto mt-3'>
-          <div className='text-center text-sm'>
-            <p>Timings</p>
-            <p>2-4 PM</p>
+          <div className='flex flex-row items-center justify-evenly w-[90%] mx-auto mt-3'>
+            <div className='text-center text-sm'>
+              <p>Timings</p>
+              <p>{data.timing}</p>
+            </div>
+            <img src="/event-card-kunai.png" alt="" className="h-[110%] w-auto" />
+            <div  className='text-center text-sm'>
+              <p>Prizes</p>
+              <p className='tracking-normal'>{data.prize}</p>
+            </div>
+            <img src="/event-card-kunai.png" alt="" className="h-[110%] w-auto" />
+            <div className='text-center text-sm'>
+              <p>Dates</p>
+              <p>{data.date}</p>
+            </div>
           </div>
-          <img src="/event-card-kunai.png" alt="" className="h-[110%] w-auto" />
-          <div  className='text-center text-sm'>
-            <p>Prizes</p>
-            <p>10k</p>
+
+          <div className='flex flex-col w-full items-center justify-center mt-3'>
+          
+          <button className='w-[80%] py-2 text-center bg-[#2F1414] hover:scale-[1.03] duration-[0.3s] text-white my-1 mx-1 rounded-full' onClick={() => setEventCard(true)}>More info</button>
+          <a href={data.unstop_link} target='_blank' className='w-[80%] py-2 text-center bg-[#2F1414] hover:scale-[1.03] duration-[0.3s] text-white my-1 mx-1 rounded-full'>Register Here</a>
           </div>
-          <img src="/event-card-kunai.png" alt="" className="h-[110%] w-auto" />
-          <div className='text-center text-sm'>
-            <p>Dates</p>
-            <p>11 Feb 2024</p>
-          </div>
-        </div>
-
-        <div className='flex flex-col w-full items-center justify-center mt-3'>
-        <button className='w-[80%] py-2 text-center bg-[#2F1414] hover:scale-[1.03] duration-[0.3s] text-white my-1 mx-1 rounded-full'>More info</button>
-        <button className='w-[80%] py-2 text-center bg-[#2F1414] hover:scale-[1.03] duration-[0.3s] text-white my-1 mx-1 rounded-full'>Register Here</button>
-        </div>
-        <img src="/event-card-bg.png" className='w-full h-full absolute z-[-1] top-0 left-0 rounded-md' />
+          <img src="/event-card-bg.png" className='w-full h-full absolute z-[-1] top-0 left-0 rounded-md' />
 
 
-        <img src="/event-card-mountain.png" className='absolute w-full bottom-0 z-[-1]' />
-    </div>
+          <img src="/event-card-mountain.png" className='absolute w-full bottom-0 z-[-1]' />
+      </div>
+    </>
   )
 }
 
